e7b848952f5f29c8dce71bf95786a447fd0d3c2b
md. Shanto <md.si.shoriful001@gmail.com>
2026-05-06 13:49:59 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  "projectId": "studio-9655621540-ee0f2",
+  "appId": "1:153002377750:web:e3c1679495c00c6d04eb37",
+  "apiKey": "AIzaSyD_zwTzJGsrDmpVnBgJqJ6VcRBnMy3-nUQ",
+  "authDomain": "studio-9655621540-ee0f2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "153002377750"
 };
