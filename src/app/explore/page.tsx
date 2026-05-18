68528ba9e42d5a4523045be08bcf0125e5f6916b
md. Shanto <md.si.shoriful001@gmail.com>
2026-05-18 18:19:08 +0000

    new

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,6 +1,6 @@
 
 "use client";
-
+export const dynamic = 'force-dynamic';
 import { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
